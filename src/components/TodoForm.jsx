d502d142be4be82383880c897fec9ada039e643b
Partha Bhowmik <iamparthabhowmik@gmail.com>
2023-12-29 02:47:23 +0530

    adding all the files

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState } from 'react'
+import { useTodo } from "../contexts/index"
+
+function TodoForm() {
+    const [todo, setTodo] = useState("")
+    const {addTodo} = useTodo()
+
+    const add = (e) => {
+        e.preventDefault()
+        if(!todo) return
+        addTodo({id: Date.now(), todo, completed: false})
+        setTodo("")
+    }
+
+  return (
+    <form onSubmit={add} className='flex'>
+        <input type="text"
+        placeholder='Write Todo...'
+        className='w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 hover:bg-white/30  py-1.5' 
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        />
+
+        <button type='submit'
+        className='rounded-r-lg px-3 py-1 bg-[#40d479] hover:bg-[#6ed093]  text-white shrink-0'
+        >Add
+        </button>
+    </form>
+  )
+}
+
+export default TodoForm
